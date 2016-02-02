a400bfbb4d0ae37b0fd945d770208af568248d4c
Jusqu123 <jusquu123@gmail.com>
2016-02-02 10:54:48 +0200

    koulussa tehtiin

---
+++ Demo07Koulu/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo07Koulu
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string line = null;
+            System.IO.StreamWriter outputFile = new System.IO.StreamWriter(@"d:\T1Lines.txt");
+            do
+            {
+                Console.WriteLine("Give text line(Enter Stops)");
+                line = Console.ReadLine();
+                outputFile.WriteLine(line);// Kirjoitetaan levylle
+            } while (line.Length != 0);
+            outputFile.Close();
+            try
+            {   string text = File.ReadAllText(@"d:\T1Lines.txt");
+                Console.WriteLine(text);
+            }
+          catch (FileNotFoundException)
+            {
+                Console.WriteLine("File not found");
+            }
+            finally
+            {
+                File.ReadAllLines(@"d:\T1Lines.txt");
+            }
+
+        }
+
+        }
+    }
+
---
+++ tehtävä2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo07Koulu
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nro = null;
+            System.IO.StreamWriter outputFile = new System.IO.StreamWriter(@"d:\T2Integers.txt");
+
+            Console.WriteLine("Give a random number");
+            do
+            {
+               
+                nro = Console.ReadLine();
+                outputFile.WriteLine(nro);// Kirjoitetaan levylle
+            } while (nro. != 0);
+
+            outputFile.WriteLine(nro);
+                outputFile.Close();
+            }
+
+        }
+    }
+}
